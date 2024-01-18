9324a52e13ebdd69f34e45af76a0b6ac994e1f8e
ammarr <47503742+ahmdammarr@users.noreply.github.com>
2024-01-19 01:37:44 +0400

    feat: set search param

--- src/app/ui/components/blocks/search/search.tsx
+++ src/app/ui/components/blocks/search/search.tsx
@@ -1,20 +1,45 @@
+"use client"
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import BaseInput from "../../base/base-input";
 import BaseButton from "../../base/base-button";
 import { toRem } from "@app/utils/to-rem";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search() {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { push } = useRouter();
+  function handleSearch(keyword: string) {
+    const params = new URLSearchParams(searchParams);
+    if (keyword) {
+      params.set("query", keyword);
+    } else {
+      params.delete("query");
+    }
+    push(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <Flex
       w="80%"
-      maxH={toRem(48)}
+      height={toRem(48)}
       justifyContent={"space-between"}
       alignItems={"center"}
       mt="sm"
     >
-      <BaseInput placeholder="Search for a repo" w={"80%"} h="100%" />
-      <BaseButton title="Search" h="100%" />
+      <BaseInput
+        placeholder="Search for a repo"
+        w={"80%"}
+        h="100%"
+        onChange={(e) => setSearchKeyword(e.target.value)}
+      />
+      <BaseButton
+        title="Search"
+        h="100%"
+        onClick={() => handleSearch(searchKeyword)}
+      />
     </Flex>
   );
 }
