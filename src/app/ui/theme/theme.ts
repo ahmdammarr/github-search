e8dade5f80cd7f2f48f5a9a3804b025f666d4db6
ammarr <47503742+ahmdammarr@users.noreply.github.com>
2024-01-18 19:51:35 +0400

    feat: add spacing

--- src/app/ui/theme/theme.ts
+++ src/app/ui/theme/theme.ts
@@ -1,7 +1,10 @@
-import { DeepPartial, extendTheme, Theme } from '@chakra-ui/react';
-import {colors} from './colors'
-import {typography} from './typography'
+import { DeepPartial, extendTheme, Theme } from "@chakra-ui/react";
+import { colors } from "./colors";
+import { typography } from "./typography";
+import { space } from "./spacing";
+
 export const theme: DeepPartial<Theme> = extendTheme({
-    typography,
-    colors
+  typography,
+  colors,
+  space,
 });
