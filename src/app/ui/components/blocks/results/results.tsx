c598496eea7d19e4859ddcea30575cf59fa6d954
ammarr <47503742+ahmdammarr@users.noreply.github.com>
2024-01-19 11:06:03 +0400

    refactor: fetch more repos

--- src/app/ui/components/blocks/results/results.tsx
+++ src/app/ui/components/blocks/results/results.tsx
@@ -16,7 +16,7 @@ export default async function Results({ query }: { query: string }) {
     url: "https://api.github.com/graphql",
     query: `
         query SearchQuery($search: String!, $after: String){
-            search(query:$search, type: REPOSITORY, first: 10, after: $after) {
+            search(query:$search, type: REPOSITORY, first: 100, after: $after) {
               pageInfo {
                 endCursor
                 hasNextPage
